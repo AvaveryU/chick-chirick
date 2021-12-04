6cc852ef11a58509495fa123f54f30e537d35e03
AvaveryU <juliya.belka.19@yandex.ru>
2021-12-04 13:21:08 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,64 @@
 const sectLead = document.querySelector('.lead');
-const blockCardsLead = sectLead.querySelector('.radio');
-const radioBtnLead = blockCardsLead.querySelectorAll('.radio__check');
 const cardsLead = sectLead.querySelectorAll('.lead__card');
+const Btn = sectLead.querySelectorAll('.radio__item');
 
-function cardOpen (card) {
-    card.classList.remove('lead__card_close')
-}
-radioBtnLead.addEventListener("click", function () {
-    cardOpen(cardsLead)
+const sectLeadCards = document.querySelector('.lead__cards');
+const cardsLeadCards = sectLeadCards.querySelectorAll('.lead__card');
+const BtnLeadCards = sectLeadCards.querySelectorAll('.radio__item');
+
+const sectMobile = document.querySelector('.mobile');
+const cardsMobile = sectMobile.querySelectorAll('.mobile__photo');
+const BtnMobile = sectMobile.querySelectorAll('.radio__item');
+
+Btn.forEach((item, index) => {
+  item.addEventListener('click', (evt) => {
+    const activeBtn = sectLead.querySelector('.radio__item_check');
+    activeBtn.classList.remove('radio__item_check');
+    evt.target.classList.add('radio__item_check');
+    cardsLead.forEach((elem, i) => {
+      if (index === i) {
+        elem.classList.remove('lead__card_close');
+      } else {
+        elem.classList.add('lead__card_close');
+      }
+    });
   });
-/*
-radioBtnLead.addEventListener('click', () => cardsLead.classList.add('lead__card_close'));
-*/
+});
+BtnLeadCards.forEach((item, index) => {
+  item.addEventListener('click', (evt) => {
+    const activeBtn = sectLeadCards.querySelector('.radio__item_check');
+    activeBtn.classList.remove('radio__item_check');
+    evt.target.classList.add('radio__item_check');
+    cardsLeadCards.forEach((elem, i) => {
+      if (index === i) {
+        elem.classList.remove('lead__card_close');
+      } else {
+        elem.classList.add('lead__card_close');
+      }
+    });
+  });
+});
+BtnMobile.forEach((item, index) => {
+  item.addEventListener('click', (evt) => {
+    const activeBtn = sectMobile.querySelector('.radio__item_check');
+    activeBtn.classList.remove('radio__item_check');
+    evt.target.classList.add('radio__item_check');
+    cardsMobile.forEach((elem, i) => {
+      if (index === i) {
+        elem.classList.remove('lead__card_close');
+      } else {
+        elem.classList.add('lead__card_close');
+      }
+    });
+  });
+});
+/*функция для активной/неактивной кнопки*/
+/*function handleClickFunction(button) {
+  button.classList.toggle('radio__item_check');
+};
+
+Btn.forEach(function (button) {
+  button.addEventListener('click', function() {
+    button.classList.toggle('radio__item_check');
+  });
+});*/
